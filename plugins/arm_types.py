40d6fe359e1a3f3a1d665ad6e337b372050f78f7
Marko Caklovic <2349514C@student.gla.ac.uk>
2018-09-03 00:50:22 +0100

    Saving work, working on implementing ARM instructions

--- plugins/arm_types.py
+++ plugins/arm_types.py
@@ -15,6 +15,8 @@ class ARMTypes(IPlugin):
             self.valid_chars_map[c] = True
 
         self.registered_types = {
+            "int_8_bits_absolute": True,
+            "int_12_bits_offset": True,
             "int_12_bits_constrained": True,
         }
 
@@ -35,6 +37,31 @@ class ARMTypes(IPlugin):
         else:
             return False
 
+    def chars_int_8_bits_absolute(self):
+        return self.valid_chars_map
+
+    def verify_int_8_bits_absolute(self, int_string):
+        parsed_int = self.parse_int(int_string)
+        if parsed_int is None:
+            return False
+        elif -128 <= parsed_int <= 255:
+            return True
+        else:
+            return False
+
+    def chars_int_12_bits_offset(self):
+        return self.valid_chars_map
+
+    def verify_int_12_bits_offset(self, int_string):
+        parsed_int = self.parse_int(int_string)
+        parsed_int = parsed_int * -1
+        if parsed_int is None:
+            return False
+        elif -2048 <= parsed_int <= 4095:
+            return True
+        else:
+            return False
+
     def parse_int(self, int_string: str) -> Optional[int]:
         if int_string.startswith("#"):
             stripped_string = int_string[1:]
@@ -50,4 +77,22 @@ class ARMTypes(IPlugin):
             b = BitArray(int=parsed_int, length=16)
         else:
             b = BitArray(uint=parsed_int, length=16)
+        return b.bin
+
+    def emit_int_8_bits_absolute(self, int_string):
+        parsed_int = self.parse_int(int_string)
+        if parsed_int < 128:
+            b = BitArray(int=parsed_int, length=8)
+        else:
+            b = BitArray(uint=parsed_int, length=8)
+        b.prepend('0b0000')
+        return b.bin
+
+    def emit_int_12_bits_offset(self, int_string):
+        parsed_int = self.parse_int(int_string)
+        parsed_int = parsed_int * -1
+        if parsed_int < 2048:
+            b = BitArray(int=parsed_int, length=12)
+        else:
+            b = BitArray(uint=parsed_int, length=12)
         return b.bin
